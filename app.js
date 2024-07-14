fcc658469a1f882292226f1cccf257a44342580f
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-13 21:13:30 -0600

    Change app error response to Json instead of view

--- app.js
+++ app.js
@@ -37,9 +37,12 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  // return error JSON response
+  if (req.accepts("json")) {
+    res.json({ message: err.message, error: err });
+  } else {
+    res.render("error");
+  }
 });
 
 module.exports = app;
