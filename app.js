c1d914a197cf65e63c0626a2fa05daab371e6503
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-16 11:49:24 -0600

    Deleted view engine (jade)

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ let app = express();
 require("./config/database");
 app.use(cors());
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
