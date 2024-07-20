5f9116127d92e826581d08444639da2b578f3469
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-20 15:34:06 -0600

    Modified User model

--- models/user.js
+++ models/user.js
@@ -2,15 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   phone: String,
+  age: Number,
   role: { type: String, enum: ["client", "provider", "admin"], required: true },
-  address: String,
-  preferences: {
-    notifications: { type: Boolean, default: true },
-  },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
