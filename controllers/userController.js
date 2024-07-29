37b110526f6082e0fd9ce7148aef676d72c1e126
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-28 22:14:01 -0600

    Create login controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,35 @@
 const User = require("../models/user");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const issueJwt = require("../utils/issueJwt");
 
 exports.signup = async (req, res) => {
   res.send("Crear un nuevo usuario");
 };
 
 exports.login = async (req, res) => {
-  //recuperar citas, servicios
-  res.send("Crear un nuevo usuario");
+  const user = await User.findOne({ username: req.body.email });
+
+  if (!user) {
+    res.status(401).json({ success: false, message: "Invalid email" });
+  }
+
+  const isValid = await bcrypt.compare(req.body.password, user.password);
+
+  if (isValid) {
+    const jwt = utils.issueJwt(user);
+
+    res.status(201).json({
+      success: true,
+      msg: "User logged in successfully",
+      user: user,
+      token: jwt.token,
+      expiresIn: jwt.expires,
+    });
+  } else {
+    res.status(401).json({ success: false, message: "Invalid password" });
+  }
 };
 
 // Obtener un usuario por su ID
