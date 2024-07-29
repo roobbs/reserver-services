13cd0324b452752de9a67162121ea0b58cb4c790
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-29 17:32:46 -0600

    Added googleId for user model

--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@ const userSchema = new Schema({
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String },
+  googleId: { type: String },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
