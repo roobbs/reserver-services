03206f0409b523b7634db7091df995e108d09c6b
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-03 14:19:44 -0600

    Added service_provider field to user model

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String },
   googleId: { type: String },
+  service_provider: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
