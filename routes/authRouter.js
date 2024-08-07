9c67fa1a64b9fe164135391b96da16ab46fea5ba
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-07 17:19:11 -0600

    Sned business when signing up with google

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,16 +2,19 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 const { issueJwt } = require("../utils/issueJwt");
+const ServiceProvider = require("../models/serviceProvider");
+const asyncHandler = require("express-async-handler");
 
 router.get(
   "/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
-
+// asyncHandler(async (req, res) => {
 router.get(
   "/google/callback",
   passport.authenticate("google", { session: false }),
-  (req, res, next) => {
+  asyncHandler(async (req, res, next) => {
+    console.log("entrando");
     try {
       if (!req.user) {
         throw new Error("User not authenticated");
@@ -19,23 +22,22 @@ router.get(
 
       const jwt = issueJwt(req.user);
 
-      // res.status(201).json({
-      //   success: true,
-      //   msg: "User logged in successfully with Google",
-      //   user: req.user,
-      //   token: jwt.token,
-      //   expiresIn: jwt.expires,
-      // });
+      const businessResponse = await ServiceProvider.findOne({
+        userId: req.user._id,
+      });
+      console.log("business response:" + businessResponse);
+      const business = businessResponse ? businessResponse : null;
 
       res.redirect(
         `http://localhost:5173/auth?token=${jwt.token}&expiresIn=${
           jwt.expires
-        }&user=${JSON.stringify(req.user)}`
+        }&user=${JSON.stringify(req.user)}&business=${JSON.stringify(business)}`
       );
     } catch (error) {
+      console.log(error);
       next(error);
     }
-  }
+  })
 );
 
 module.exports = router;
