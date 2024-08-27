bdbb886722c986baf49bff0cc57a8a709b163276
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-26 20:58:51 -0600

    Modified appointment router

--- routes/appointmentRouter.js
+++ routes/appointmentRouter.js
@@ -4,7 +4,7 @@ const AppointmentController = require("../controllers/appointmentController");
 
 router.post("/", AppointmentController.createAppointment); // Crea una nueva cita
 router.get("/:appointmentId", AppointmentController.getAppointmentById); // Obtiene una cita por su ID
-router.put("/:appointmentId", AppointmentController.updateAppointment); // Actualiza una cita por su ID
+router.put("/:appointmentId/cancel", AppointmentController.cancelAppointment);
 router.delete("/:appointmentId", AppointmentController.deleteAppointment); // Elimina una cita por su ID
 
 module.exports = router;
