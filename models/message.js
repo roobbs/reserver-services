0c2d992fca8e5beeec77b7de9b6c88e67c60fd11
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-09-02 17:43:17 -0600

    Modified Message schema

--- models/message.js
+++ models/message.js
@@ -4,9 +4,18 @@ const Schema = mongoose.Schema;
 const messageSchema = new Schema({
   senderId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   receiverId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  content: String,
+  conversationId: {
+    type: Schema.Types.ObjectId,
+    ref: "Conversation",
+    required: true,
+  },
+  content: { type: String, required: true },
+  status: {
+    type: String,
+    enum: ["sent", "delivered", "read"],
+    default: "sent",
+  },
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Message", messageSchema);
