a274a6f61ba1d8c5085bc7fec42077c260b6a3a8
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-09-03 11:58:47 -0600

    Fix error in conversation schema

--- models/conversation.js
+++ models/conversation.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema;
 
 const conversationSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  business: { type: Schema.Types.ObjectId, ref: "Business", required: true },
+  business: {
+    type: Schema.Types.ObjectId,
+    ref: "ServiceProvider",
+    required: true,
+  },
   lastMessage: { type: Schema.Types.ObjectId, ref: "Message" },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
