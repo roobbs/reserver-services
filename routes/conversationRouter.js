c6cc030e0dce8a06f0238d929f7701693fca517a
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-09-04 15:23:14 -0600

    Added new route to Conversation Router

--- routes/conversationRouter.js
+++ routes/conversationRouter.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const ConversationController = require("../controllers/conversationController");
 
 router.post("/", ConversationController.createConversation);
+router.get(
+  "/:conversationId/messages",
+  ConversationController.getMessagesFromConversation
+);
 
 module.exports = router;
